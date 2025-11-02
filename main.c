7a14e18c0ea467759d5e05ff067ebb47178f7924
Kilomaroon <milesmandevilla@gmail.com>
2025-11-01 22:30:31 -0600

    project setup

---
+++ main.c
@@ -0,0 +1,12 @@
+/**
+ * @file main.c
+ * @author mimaroon
+ * @date 2025-11-02
+ * @brief Main function
+ */
+int main(){
+
+    // Add your code here and press Ctrl + Shift + B to build
+
+    return 0;
+}
