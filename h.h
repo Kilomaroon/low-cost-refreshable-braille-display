6fe8ea8fcf7cf4180fda63f1982412728bfa3e3e
Miles Taylor <milesmandevilla@gmail.com>
2026-01-01 12:26:52 -0700

    Ran a basic loop in main.c to discern functionality of decoder component. Created placeholder header file.

---
+++ h.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   h.h
+ * Author: mimim
+ *
+ * Created on January 1, 2026, 12:19 PM
+ */
+
+#ifndef H_H
+#define	H_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* H_H */
+
--- main.c
+++ main.c
@@ -1,12 +1,54 @@
-/**
- * @file main.c
- * @author mimaroon
- * @date 2025-11-02
- * @brief Main function
+/* 
+ * File:   main.c
+ * Author: mimim
+ *
+ * Created on November 9, 2025, 11:25 AM
  */
-int main(){
 
-    // Add your code here and press Ctrl + Shift + B to build
 
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define F_CPU 14745600UL
+
+
+#define G0  PD0
+#define G2  PC5
+#define AS0 PC2
+#define AS1 PC3
+#define AS2 PC4
+
+int main(void) {
+    DDRC |= (1<<G2)|(1<<AS0)|(1<<AS1)|(1<<AS2);
+    DDRD |= (1<<G0);
+    
+    // G0:2 = 0b0X1
+    PORTD &= ~(1<<G0);
+    PORTC |= (1<<G2);
+    
+    
+    
+    PORTC |= (0b00010100);
+    
+    uint8_t x = 0b00000000;
+    
+    while(1) { 
+        // AS0:2 = 000 -> &= ~ 0x8F
+        PORTC &= ~((1<<AS1)|(1<<AS2)|(1<<AS0));
+        PORTC |= (0b00001000);
+        _delay_ms(10000); 
+        x = x + 1;
+        if (x > 0b101){
+            x = 0b000;
+        }
+
+    }
+
+
+    
     return 0;
 }
+
